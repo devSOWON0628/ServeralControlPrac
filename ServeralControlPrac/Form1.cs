6ba542df5f01f07ce3e4cf90624d22121f893ae8
소원 박 <s2018s29@e-mirim.hs.kr>
2020-07-21 15:01:11 +0900

    CheckBox, Button 추가하고 내용 넣기

--- ServeralControlPrac/Form1.cs
+++ ServeralControlPrac/Form1.cs
@@ -25,6 +25,44 @@ namespace ServeralControlPrac
             l1.Location = new Point(230, 80);
             Controls.Add(l1);
             l1.Click += LabelClick;
+
+            CheckBox cb1 = new CheckBox();
+            CheckBox cb2 = new CheckBox();
+            CheckBox cb3 = new CheckBox();
+            Button btn1 = new Button();
+
+            cb1.Location = new Point(10, 10);
+            cb2.Location = new Point(10, 40);
+            cb3.Location = new Point(10, 70);
+            btn1.Location = new Point(10, 100);
+            btn1.Size = new Size(120, 40);
+
+            cb1.Text = "고구려";
+            cb2.Text = "김치";
+            cb3.Text = "토마토";
+            btn1.Text = "감자";
+
+            btn1.Name = "combo";
+            btn1.Click += btn1Click;
+
+            Controls.Add(cb1);
+            Controls.Add(cb2);
+            Controls.Add(cb3);
+            Controls.Add(btn1);
+
+        }
+
+        private void btn1Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            foreach(var item in Controls)
+            {
+                if((item is CheckBox)&&(((CheckBox)item).Checked))
+                {
+                    list.Add(((CheckBox)item).Text);
+                }
+            }
+      
         }
 
         private void LabelClick(object sender, EventArgs e)
