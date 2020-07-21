94f52264b17b3d97f1a14c28e7f790652bc0b272
소원 박 <s2018s29@e-mirim.hs.kr>
2020-07-21 14:43:58 +0900

    label, linklabel 사용법

--- ServeralControlPrac/Form1.cs
+++ ServeralControlPrac/Form1.cs
@@ -17,6 +17,13 @@ namespace ServeralControlPrac
             InitializeComponent();
             Label label = new Label();
             label.Text = "원하는 글자 출력";
+            label.Location = new Point(400,10);
+            Controls.Add(label);
+
+            LinkLabel l1 = new LinkLabel();
+            l1.Text = "구글";
+            l1.Location = new Point(230, 80);
+            Controls.Add(l1);
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
