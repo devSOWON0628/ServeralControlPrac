642a9472c7640fc1af5f1a8b39e05ed2aa9e694a
소원 박 <s2018s29@e-mirim.hs.kr>
2020-07-21 14:39:48 +0900

    초기 커밋.

---
+++ ServeralControlPrac/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServeralControlPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Label label = new Label();
+            label.Text = "원하는 글자 출력";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+    }
+}
